0eba3fc8f137aebd2bbff692c5b3efe726686afb
belf33 <d16m10g33>
2023-01-10 23:40:21 +0300

    contract refactoring

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -42,10 +42,7 @@ contract FundMe {
 
     function fund() public payable {
         if ( msg.value.getConversionRate(s_priceFeed) < MINIMUM_USD) revert FundMe_NotEnoughETH();
-        // require(
-        //     msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
-        //     'You need to spend more ETH!'
-        // );
+        
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);
     }
